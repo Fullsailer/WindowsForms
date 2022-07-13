44aeceba8bf2be3c747834dd31a6d95c8499bcd4
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2022-07-13 11:37:27 -0400

    Initial Commit
    Created New Project for building out Windows Forms.
    Added Basic List Box Form
    Added Basic Dialog Form
    Added Basic Menu and Toolbar Form
    Added Basic User Controls Form

---
+++ WindowsFormsDemos/WindowsFormsDemos/CustomDialogs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDemos
+{
+    public partial class CustomDialogs : Form
+    {
+        public CustomDialogs()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WindowsFormsDemos/WindowsFormsDemos/Lifecycle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDemos
+{
+    public partial class Lifecycle : Form
+    {
+        public Lifecycle()
+        {
+            InitializeComponent();
+        }
+
+        //Loads data into form.
+        private void Lifecycle_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Oranges");
+            listBox1.Items.Add("Grapes");
+            listBox1.Items.Add("Bananas");
+            listBox1.Items.Add("Peaches");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = listBox1.Text;
+        }
+    }
+}
