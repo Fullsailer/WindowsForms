680f9d74039a2c38999a4a2b03b0e6d612da621e
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2022-07-18 17:20:44 -0400

    Created New Show Form project with two forms.
    I created a new project to test out why the show and show dialog where not working in my other forms? It has something to do with how my project is set up. I need to clean it up or rebuild them.
    I was able to set up the regular show form and the show dialog that will not let you move between the two forms.

---
+++ ShowForm/ShowForm/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShowForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            //f.Show(); This just shows a new form window.
+            f.ShowDialog(); // This shows a new form dialog that won't let you close it. 
+        }
+    }
+}
--- ShowForm/ShowForm/Program.cs
+++ ShowForm/ShowForm/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsDemos
+namespace ShowForm
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace WindowsFormsDemos
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ShowForm.Form1());
+            Application.Run(new Form1());
         }
     }
 }
