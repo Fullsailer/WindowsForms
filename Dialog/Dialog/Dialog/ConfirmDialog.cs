501e25fe541fbc893926ac062f1f8fa631f2dd74
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2022-07-22 10:40:01 -0400

    Created new Dialog Project and User Controls Project
    I build a new user control project and a dialog project.
    The Dialog project bring up two dialogs one for error handling for no selected items from list box. The second one is a confirm close dialog when closing the application.

---
+++ Dialog/Dialog/Dialog/ConfirmDialog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dialog
+{
+    public partial class ConfirmDialog : Form
+    {
+        public ConfirmDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfirmDialog_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ UserControls/UserControls/UserControls/PersonControl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace UserControls
+{
+    public partial class PersonControl : UserControl
+    {
+        public PersonControl()
+        {
+            InitializeComponent();
+        }
+
+        private Person _person;
+
+        public Person Person
+        {
+            get
+            {
+                _person.FirstName = FirstNametextBox.Text;
+                _person.LastName = LastNametextBox.Text;
+                _person.Age = Convert.ToInt32(AgetextBox.Text);
+                return _person;
+            }
+            set
+            {
+                FirstNametextBox.Text = value.FirstName;
+                LastNametextBox.Text = value.LastName;
+                AgetextBox.Text = value.Age.ToString();
+                _person = value;
+            }
+        }
+    }
+}
