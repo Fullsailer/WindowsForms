5427ddaa0fbde3bffd6673233a4df76163390627
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2022-07-25 12:54:00 -0400

    Update The MDI Application Finished Build is fully functional.
    Deleted the Menu Strip Items Help and Tools
    Added Menu strip Items Window with Cascade and Tile Horizontally and Vertically
    Added Methods for the rest of the menu.
    Added Toolbar with the icon that connects to the event action methods for each feature.

--- Dialog/MDI/MDI/MainForm.cs
+++ Dialog/MDI/MDI/MainForm.cs
@@ -28,12 +28,129 @@ namespace MDI
         }
 
 
-        private void NewToolStripMenuItem_Click(object sender, EventArgs e)
+        private void NewToolStripMenuItem_Click(object sender, EventArgs e)  
         {
             var childForm = new ChildForm();
             childForm.MdiParent = this;
-            //childForm.Text = "New Document " + ++_counter;
+            childForm.Text = "New Document " + ++_counter;
             childForm.Show();
+        }
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "Rich text files|*.rtf";
+            var result = dialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                var childForm = new ChildForm();
+                childForm.documentTextBox.LoadFile(dialog.FileName);
+                childForm.Text = dialog.FileName;
+                childForm.MdiParent = this;
+                childForm.Show();
+            }
+        }
+
+        private void CloseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                this.ActiveMdiChild.Close();
+            }
+        }
+
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                var childForm = (ChildForm)this.ActiveMdiChild;
+
+                var dialog = new SaveFileDialog();
+                dialog.Filter = "Rich text files|*.rtf";
+                dialog.AddExtension = true;
+                var result = dialog.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    childForm.documentTextBox.SaveFile(dialog.FileName);
+                    childForm.Text = dialog.FileName;
+                }
+            }
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void UndoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                var childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.documentTextBox.Undo();
+            }
+        }
+
+        private void RedoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                var childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.documentTextBox.Redo();
+            }
+        }
+
+        private void CutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                var childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.documentTextBox.Cut();
+            }
+        }
+
+        private void CopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                var childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.documentTextBox.Copy();
+            }
+        }
+
+        private void PasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                var childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.documentTextBox.Paste();
+            }
+        }
+
+        private void SelectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                var childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.documentTextBox.SelectAll();
+            }
+        }
+
+        private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void TileHorizontallyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void TileVerticallyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileVertical);
         }
     }
 }
