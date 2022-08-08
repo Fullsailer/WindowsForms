3835c70f7a803732a1e1c6aac7af4434e428e8f5
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2022-08-08 12:54:17 -0400

    Built a Tabs Form Window that adds colors based on the tab clicked and adds, removes, or clears all Tabs.
    I build a tabs form window.
    I added colors to the tabs backgrounds.
    I added buttons to add new tab, removed selected tab, and remove all tabs.

---
+++ TabsForm/TabsForm/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TabsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //tabControl1.SelectTab("tabPage2"); //Selects the tab to show at launch
+            tabControl1.SelectedIndex = 2; // Zero indexed so 2 is actually tab 3;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TabPage tpage = new TabPage("Page");
+            Label l1 = new Label();
+            l1.Text = "Tab";
+            tpage.Controls.Add(l1);
+            tabControl1.TabPages.Add(tpage);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Remove(tabControl1.SelectedTab);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Clear();
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedTab == tabPage1)
+            {
+                tabPage1.BackColor = Color.Green;
+            }
+            else if (tabControl1.SelectedTab == tabPage2)
+            {
+                tabPage2.BackColor = Color.Yellow;
+            }
+            else if (tabControl1.SelectedTab == tabPage3)
+            {
+                tabPage3.BackColor = Color.Blue;
+            }
+        }
+    }
+}
