9967537ab4bc7093455e18224adeb442747c873d
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2022-10-05 10:45:27 -0400

    Added Views tot he Tool Bar
    Added the Large, Small, and Detail view to the tool strip.

--- ExplorerApplication/Explorer/Explorer/ExplorerForm.cs
+++ ExplorerApplication/Explorer/Explorer/ExplorerForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,7 +27,7 @@ namespace Explorer
                 listView1.Columns.Add("Size", 75, HorizontalAlignment.Right);
                 ViewToolStripComboBox.SelectedIndex = 0;
 
-                var docs = new TreeNode("Me Documents");
+                var docs = new TreeNode("My Documents");
                 docs.Tag = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
                 treeView1.Nodes.Add(docs);
 
@@ -53,6 +54,59 @@ namespace Explorer
                 var lastWrite = file.LastWriteTime;
                 item.SubItems.Add(lastWrite.ToShortDateString() + " " + lastWrite.ToShortTimeString());
                 item.SubItems.Add(Math.Ceiling(file.Length / 1024.0) + " KB");
+
+                if (!(LargeImageList.Images.ContainsKey(file.Extension)))
+                {
+                    var thisIcon = Icon.ExtractAssociatedIcon(file.FullName);
+                    SmallImageList.Images.Add(file.Extension, thisIcon);
+                    LargeImageList.Images.Add(file.Extension, thisIcon);
+                }
+
+                item.ImageKey = file.Extension;
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void ViewToolStripComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (ViewToolStripComboBox.Text)
+            {
+                case "Large Icons":
+                    listView1.View = View.LargeIcon;
+                    break;
+                case "Small Icons":
+                    listView1.View = View.SmallIcon;
+                        break;
+                case "Details":
+                    listView1.View = View.Details;
+                    break;
+            }
+        }
+
+        private void GetFolder(TreeNode node)
+        {
+            var dir = new DirectoryInfo(node.Tag.ToString());
+
+
+            try
+            {
+                foreach (var childDir in dir.GetDirectories())
+                {
+                    if (childDir.Attributes.HasFlag(FileAttributes.Hidden))
+                    {
+                        continue;
+                    }
+
+                    var childNode = new TreeNode(childDir.Name);
+                    childNode.Tag = childDir.FullName;
+                    node.Nodes.Add(childNode);
+
+                    GetFolder(childNode);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text);
             }
         }
     }
